e692174d5037ddcf784b7858c89345d1fca89516
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-02 23:24:19 +0300

    initial commit, setup main screen

---
+++ main.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+
+BACKGROUND_COLOR = "#C4D9FF"
+
+# Setup the main screen
+window= Tk()
+window.title("Miles to KM Converter")
+window.minsize(width=100, height=100)
+window.config(padx=30, pady=30, bg=BACKGROUND_COLOR)
+
+
+
+window.mainloop()
