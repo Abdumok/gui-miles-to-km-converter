9d84fbbab3be9b73e47f13883f909cee4ff89c9c
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-03-03 23:08:22 +0300

    add labels and button to the program screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, Entry, Label, Button
 
 BACKGROUND_COLOR = "#C4D9FF"
 
@@ -7,6 +7,24 @@ window= Tk()
 window.title("Miles to KM Converter")
 window.minsize(width=100, height=100)
 window.config(padx=30, pady=30, bg=BACKGROUND_COLOR)
+
+user_input= Entry()
+user_input.grid(column= 1, row= 0)
+
+miles_label= Label(text= "Miels", padx=10, bg=BACKGROUND_COLOR)
+miles_label.grid(column= 2, row= 0)
+
+is_equal= Label(text="Is equal to:", bg=BACKGROUND_COLOR)
+is_equal.grid(column= 0, row=1)
+
+km_label= Label(text= "KM", bg= BACKGROUND_COLOR)
+km_label.grid(column= 2,row= 1)
+
+result_label= Label(text="0", padx=10, bg= BACKGROUND_COLOR)
+result_label.grid(column= 1, row= 1)
+
+button= Button(text="Calculate")
+button.grid(column=1, row= 2)
 
 
 
